85d49d1a2accc46fc65a5ed0fcee626920ce40da
Igor Ljevak <igorljevak@gmail.com>
2021-08-30 16:16:37 +0200

    uppdate all sites

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+// Mobile nav
+const wrapperMenu = document.querySelector('.wrapper-menu');
+const mobileNav = document.querySelector('.open-nav');
+
+wrapperMenu.addEventListener('click', function () {
+  wrapperMenu.classList.toggle('open');
+
+  if (mobileNav.style.display === 'block') {
+    mobileNav.style.display = 'none';
+  } else {
+    mobileNav.style.display = 'block';
+  }
+});
+
+// Job tabs
+function _class(name) {
+  return document.getElementsByClassName(name);
+}
+
+let tabPanes = _class('tab-header')[0].getElementsByTagName('div');
+
+for (let i = 0; i < tabPanes.length; i++) {
+  tabPanes[i].addEventListener('click', function () {
+    _class('tab-header')[0].getElementsByClassName('active')[0].classList.remove('active');
+    tabPanes[i].classList.add('active');
+
+    _class('tab-indicator')[0].style.top = `calc(80px + ${i * 50}px)`;
+
+    _class('tab-content')[0].getElementsByClassName('active')[0].classList.remove('active');
+    _class('tab-content')[0].getElementsByTagName('div')[i].classList.add('active');
+  });
+}
+
+// contact form
 // Listen to Submit
 document.querySelector('.contact-form').addEventListener('submit', submitForm);
 
