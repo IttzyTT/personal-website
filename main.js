c48e1946c3a4b1feea570872f9ec540b1429e059
Igor Ljevak <igorljevak@gmail.com>
2021-08-31 13:37:47 +0200

    contact form

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Mobile nav
 const wrapperMenu = document.querySelector('.wrapper-menu');
 const mobileNav = document.querySelector('.open-nav');
+let mobileLinks = document.querySelectorAll('.links-mobile ul li a');
 
 wrapperMenu.addEventListener('click', function () {
   wrapperMenu.classList.toggle('open');
@@ -11,6 +12,14 @@ wrapperMenu.addEventListener('click', function () {
     mobileNav.style.display = 'block';
   }
 });
+
+mobileLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    mobileNav.style.display = 'none';
+    wrapperMenu.classList.toggle('open');
+  });
+});
+// ----------------------------------------------------------------------------
 
 // Job tabs
 function _class(name) {
@@ -32,30 +41,25 @@ for (let i = 0; i < tabPanes.length; i++) {
 }
 
 // contact form
-// Listen to Submit
-document.querySelector('.contact-form').addEventListener('submit', submitForm);
-
-function submitForm(e) {
-  e.preventDefault();
-
-  // Get input values
-  let name = document.querySelector('.name').value;
-  let email = document.querySelector('.email').value;
-  let subject = document.querySelector('.subject').value;
-  let message = document.querySelector('.message').value;
-
-  document.querySelector('.contact-form').reset();
-
-  sendEmail(name, email, subject, message);
-}
 
 // Send email Info
-function sendEmail(name, email, subject, message) {
-  Email.send({
-    SecureToken: '145731b8-ff91-4382-8837-d130c9cf8aa3',
-    To: 'igorljevak@gmail.com',
-    From: 'igorljevak@gmail.com',
-    Subject: `${subject}`,
-    Body: `Name: ${name} <br/> Email: ${email} <br/> Subject: ${subject} <br/> Message: ${message}`,
-  }).then((message) => alert('mail sent seccessfully'));
-}
+//get the form by its id
+const form = document.getElementById('contact-form');
+
+const formEvent = form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('submitted');
+
+  let mail = new FormData(form);
+
+  sendMail(mail);
+});
+
+const sendMail = (mail) => {
+  fetch('https://igorljevak.com/send', {
+    method: 'post',
+    body: mail,
+  }).then((response) => {
+    return response.json();
+  });
+};
